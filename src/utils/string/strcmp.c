1dc547364fd78023b0475f77a3eb1bfffd6dcabd
luna <luna7111@tuta.io>
2025-04-27 21:09:13 +0200

    I ended up deleting the check that was intended to avoid segfaults because it caused incoherent behaviour

--- src/utils/string/strcmp.c
+++ src/utils/string/strcmp.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strcmp.c                                           :+:      :+:    :+:   */
+/*   strcmp.c                                       .' .-'`        *          */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmagro-r <nmagro-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 18:59:31 by nmagro-r          #+#    #+#             */
-/*   Updated: 2025/04/27 19:38:57 by nmagro-r         ###   ########.fr       */
+/*   Updated: 2025/04/27 20:58:14 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ int	ft_strcmp(const char *s1, const char *s2)
 {
 	int	i;
 
-	if (!s1 || !s2)
-		return(-1);
 	i = 0;
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
@@ -30,13 +28,15 @@ int	ft_strcmp(const char *s1, const char *s2)
 // i of both strings are the same and are not '\0'.
 // returns the subs
 
-/*
+
 
 int main(void)
 {
-	HACER PRUEBAS!!!!!!!!!!!!!!!!!!!!
-	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	printf("%d\n", ft_strcmp(NULL, "a"));
+	printf("%d\n", ft_strcmp("b", "a")); // 1
+	printf("%d\n", ft_strcmp("a", "a")); // 0
+	printf("%d\n", ft_strcmp("aa", "ab")); // -1
+	printf("%d\n", ft_strcmp("aa", "abb")); // -1
+	printf("%d\n", ft_strcmp("0a", "")); // 48
+	printf("%d\n", ft_strcmp(NULL, "a")); //segfault
 }
-*/
+
