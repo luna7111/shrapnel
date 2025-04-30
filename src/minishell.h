83d6f5942d1c518296be1c91086d94377ac54ff0
nmagro-r <nacho.magro.ramos@gmail.com>
2025-04-30 18:51:07 +0200

    Added substr to minishell.h

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nmagro-r <nmagro-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 23:45:58 by ldel-val          #+#    #+#             */
-/*   Updated: 2025/04/30 17:27:20 by nmagro-r         ###   ########.fr       */
+/*   Updated: 2025/04/30 18:45:03 by nmagro-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,5 +87,6 @@ size_t	ft_strlen(char *str);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strchr(char *str, char chr);
 char	*ft_strrchr(char *str, char chr);
+char	*ft_substr(char *str, unsigned int start, size_t len);
 
 #endif
