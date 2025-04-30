8d905ef99111e9553d425ead610b13da7086d219
luna <luna7111@tuta.io>
2025-04-30 12:39:08 +0200

    Added main function and init_data static function

---
+++ src/main_loop.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                    _.._  .           .     */
+/*   main_loop.c                                    .' .-'`        *          */
+/*                                                 /  /       +        *      */
+/*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
+/*                                                 \  '.___.;       +         */
+/*   Created: 2025/04/30 11:43:13 by ldel-val       '._  _.'   .        .     */
+/*   Updated: 2025/04/30 12:37:05 by ldel-val          ``                     */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+static t_data	*init_data(t_gctr *gctrl)
+{
+	t_data	*data;
+
+	data = gctrl_malloc(gctrl, sizeof(t_data), PROG_BLOCK);
+	data->last_input = NULL;
+}
+
+int main(int argc, char **argv, char **env)
+{
+	t_gctrl	*gctrl;
+	t_data	*data;
+	t_iter	*iter_struct;
+
+	gctrl = gctrl_innit();
+	data = init_data(gctrl);
+	while (1)
+	{
+		iter_struct->raw_input = get_user_input(gctrl, data);
+	}
+	gctrl_terminate(gctrl);
+}
