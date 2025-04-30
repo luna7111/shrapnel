4e1e5a0f3fa217e13ccb488f2d37af0623a7f449
nmagro-r <nacho.magro.ramos@gmail.com>
2025-04-30 17:16:51 +0200

    Empezado substr pero hay casos sin contemplar y no se han hecho las pruebas (len mayor que la string, start mayor que la string str vacío o null...)

---
+++ src/utils/string/substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   substr.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmagro-r <nmagro-r@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 16:58:38 by nmagro-r          #+#    #+#             */
+/*   Updated: 2025/04/30 17:15:42 by nmagro-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+char	*ft_substr(char const *str, unsigned int start, size_t len)
+{
+	char	*result;
+
+	result = malloc((len + 1) * sizeof(char));
+	str += start;
+	result[len] = '\0';
+	while (len--)
+		result[len] = str[len];
+	return(result);
+}
+
+int main(void)
+{
+	char	str[] = "Hola mundo! que tal?";
+	printf("Original: %s\n", str);
+	printf("Substr: %s\n", ft_substr(str, 1, 10));
+}
