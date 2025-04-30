d9bdeba520b1adc3cc978a68f0eac7c040f9ccd0
nmagro-r <nacho.magro.ramos@gmail.com>
2025-04-30 17:25:13 +0200

    Added null check

--- src/utils/string/substr.c
+++ src/utils/string/substr.c
@@ -6,7 +6,7 @@
 /*   By: nmagro-r <nmagro-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 16:58:38 by nmagro-r          #+#    #+#             */
-/*   Updated: 2025/04/30 17:15:42 by nmagro-r         ###   ########.fr       */
+/*   Updated: 2025/04/30 17:25:07 by nmagro-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
 	char	*result;
 
+	if (!str)
+		return (NULL);
 	result = malloc((len + 1) * sizeof(char));
 	str += start;
 	result[len] = '\0';
