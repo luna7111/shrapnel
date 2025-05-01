b93f6475937228ff70ad5c96986f01e81ec0d4d9
nmagro-r <nacho.magro.ramos@gmail.com>
2025-05-01 18:52:40 +0200

    Added memory function prototypes to header file

--- src/minishell.h
+++ src/minishell.h
@@ -106,7 +106,13 @@ char	*ft_strrchr(char *str, char chr);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char *str, unsigned int start, size_t len);
 
+// Memory
 void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memchr(const void *s, int c, size_t n);
+int     ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 
 // Other functions
 char	*get_user_input(t_gctrl *gctrl, t_data *data);
