f71519609e515cdead66feed9ddbb9de3af3149c
luna <luna7111@tuta.io>
2025-05-02 12:56:46 +0200

    Added function env_to_node

---
+++ src/enviroment/env_new_node.c
@@ -0,0 +1,24 @@
+#include <minishell.h>
+
+t_enviroment	*env_new_node(t_gctrl *gctrl, char *raw_variable)
+{
+	t_enviroment	*node;
+	int				eq_sign_position;
+
+	eq_sign_position = ft_strchr(raw_variable, '=') - raw_variable;
+	node = gctrl_malloc(gctrl, sizeof(t_enviroment), PROG_BLOCK);
+	node->raw = ft_strdup(raw_variable);
+	node->name = ft_substr(raw_variable, 0, eq_sign_position);
+	node->content = ft_substr(raw_variable, eq_sign_position + 1, ft_strlen(raw_variable));
+	printf("raw: %s\nname: %s\ncontent: %s\n", node->raw, node->name, node->content);
+	return (NULL);
+}
+
+int main()
+{
+	t_gctrl	*gctrl;
+
+	gctrl = gctrl_init();
+	env_new_node(gctrl, "NAME=NACHETE");
+	gctrl_terminate(gctrl);
+}
