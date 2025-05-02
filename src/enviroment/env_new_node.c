e6066fe1fa27962d3a52a0a03d9b21889814ceff
luna <luna7111@tuta.io>
2025-05-03 00:24:49 +0200

    Commented main

--- src/enviroment/env_new_node.c
+++ src/enviroment/env_new_node.c
@@ -9,14 +9,15 @@ t_enviroment	*env_new_node(t_gctrl *gctrl, const char *raw_variable)
 	node = gctrl_malloc(gctrl, PROG_BLOCK, sizeof(t_enviroment));
 	node->raw = ft_strdup(raw_variable);
 	node->name = ft_substr(raw_variable, 0, eq_sign_position);
-	node->content = ft_substr(raw_variable, eq_sign_position + 1, ft_strlen(raw_variable));
+	node->content = ft_substr(raw_variable, eq_sign_position + 1,
+		ft_strlen(raw_variable));
 	gctrl_track_ptr(gctrl, node->raw, PROG_BLOCK);
 	gctrl_track_ptr(gctrl, node->name, PROG_BLOCK);
 	gctrl_track_ptr(gctrl, node->content, PROG_BLOCK);
-	printf("raw: %s\nname: %s\ncontent: %s\n", node->raw, node->name, node->content);
 	return (NULL);
 }
 
+/*
 int main()
 {
 	t_gctrl	*gctrl;
@@ -25,3 +26,4 @@ int main()
 	env_new_node(gctrl, "NAME=NACHETE");
 	gctrl_terminate(gctrl);
 }
+*/
