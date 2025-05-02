c47dc87a2498e50e0ae40f8a0e3fbbaef35e5430
luna <luna7111@tuta.io>
2025-05-03 00:20:52 +0200

    Fixed gctrl command calls syntax typos

--- src/enviroment/env_new_node.c
+++ src/enviroment/env_new_node.c
@@ -1,18 +1,18 @@
 #include <minishell.h>
 
-t_enviroment	*env_new_node(t_gctrl *gctrl, char *raw_variable)
+t_enviroment	*env_new_node(t_gctrl *gctrl, const char *raw_variable)
 {
 	t_enviroment	*node;
 	int				eq_sign_position;
 
 	eq_sign_position = ft_strchr(raw_variable, '=') - raw_variable;
-	node = gctrl_malloc(gctrl, sizeof(t_enviroment), PROG_BLOCK);
+	node = gctrl_malloc(gctrl, PROG_BLOCK, sizeof(t_enviroment));
 	node->raw = ft_strdup(raw_variable);
 	node->name = ft_substr(raw_variable, 0, eq_sign_position);
 	node->content = ft_substr(raw_variable, eq_sign_position + 1, ft_strlen(raw_variable));
-	gctrl_track_ptr(node->raw);
-	gctrl_track_ptr(node->name);
-	gctrl_track_ptr(node->content);
+	gctrl_track_ptr(gctrl, node->raw, PROG_BLOCK);
+	gctrl_track_ptr(gctrl, node->name, PROG_BLOCK);
+	gctrl_track_ptr(gctrl, node->content, PROG_BLOCK);
 	printf("raw: %s\nname: %s\ncontent: %s\n", node->raw, node->name, node->content);
 	return (NULL);
 }
