765f2b522c5fa664dfa2153be2cc44cff6991b72
luna <luna7111@tuta.io>
2025-05-03 19:51:05 +0200

    Changed env_delete_node for it to be able to delete the first node of the list

--- src/enviroment/env_delete_node.c
+++ src/enviroment/env_delete_node.c
@@ -1,10 +1,20 @@
+#include <minishell.h>
+
 void	env_delete_node(t_gctrl *gctrl, t_enviroment *list, t_enviroment *node)
 {
-	while (list->next != NULL && list->next != node)
-		list = list->next;
-	list->next = node->next;
-	gctrl_free(gctrl, next->name);
-	gctrl_free(gctrl, next->content);
-	gctrl_free(gctrl, next->raw);
-	gctrl_free(gctrl, next);
+	t_enviroment	*cursor;
+	
+	if (*list == node)
+		*list = node->next;
+	else
+	{
+		cursor = *list;
+		while (cursor->next != node && cursor->next != NULL)
+			cursor = cursor->next;
+		cursor->next = node->next;
+	}
+	gctrl_free(gctrl, node->raw);
+	gctrl_free(gctrl, node->name);
+	gctrl_free(gctrl, node->content);
+	gctrl_free(gctrl, node);
 }
