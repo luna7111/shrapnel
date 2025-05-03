31c5acf526e8c1fe69b76eee986d0e44daad42d1
luna <luna7111@tuta.io>
2025-05-03 02:47:46 +0200

    Added env_find_node function

---
+++ src/enviroment/env_find_node.c
@@ -0,0 +1,27 @@
+#include <minishell.h>
+
+t_enviroment	*env_find_node(t_enviroment *head, const char *name)
+{
+	size_t	namelen;
+
+	namelen = ft_strlen(name) + 1;
+	while (head != NULL)
+	{
+		if (ft_strncmp(name, head->name, namelen) == 0)
+			return (head);
+		head = head->next;
+	}
+	return (NULL);
+}
+/*
+int main(int argc, char **argv, char **env)
+{
+        t_gctrl                 *gctrl;
+        t_enviroment    *envlist;
+
+        gctrl = gctrl_init();
+        envlist = env_to_list(gctrl, env);
+		printf("%s\n", env_find_node(envlist, "ZSH")->content);
+        gctrl_terminate(gctrl);
+}
+*/
