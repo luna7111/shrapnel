45588892166a4460f973aae5203858cbca7fa843
luna <luna7111@tuta.io>
2025-05-05 14:39:47 +0200

    Added init_iter function

--- src/main_loop.c
+++ src/main_loop.c
@@ -4,10 +4,19 @@ static t_data	*init_data(t_gctrl *gctrl, char **env)
 {
 	t_data	*data;
 
-	data = gctrl_malloc(gctrl, sizeof(t_data), PROG_BLOCK);
+	data = gctrl_malloc(gctrl, PROG_BLOCK, sizeof(t_data));
 	data->last_input = NULL;
 	data->env = env_to_list(gctrl, env);
 	return (data);
+}
+
+static t_iter	*init_iter(t_gctrl *gctrl)
+{
+	t_iter	*iter;
+
+	iter = gctrl_malloc(gctrl, PROG_BLOCK, sizeof(t_data));
+	iter->raw_input = NULL;
+	return (iter);
 }
 
 int	main(int argc, char **argv, char **env)
@@ -18,9 +27,12 @@ int	main(int argc, char **argv, char **env)
 
 	gctrl = gctrl_init();
 	data = init_data(gctrl, env);
+	iter_struct = init_iter(gctrl);
 	while (1)
 	{
 		iter_struct->raw_input = get_user_input(gctrl, data);
+		if (!ft_strcmp(iter_struct->raw_input, "exit"))
+			break ;
 	}
 	gctrl_terminate(gctrl);
 	(void)argv;
