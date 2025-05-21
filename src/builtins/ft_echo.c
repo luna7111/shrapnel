e13878a665149fc0ca04408ac133a83cf3bfe052
nmagro-r <nacho.magro.ramos@gmail.com>
2025-05-21 12:46:15 +0200

    comentarios incluidos

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -15,6 +15,11 @@ static int	is_n_flag(char *arg)
 	}
 	return (1);
 }
+// Función que verifica si el argumento es un flag -n, permitiendo múltiples n (como -nnn).
+//Si el argumento es nulo, no empieza con -, o el segundo carácter no es n, retorna 0, no es flag válida.
+// Inicia iteración desde el tercer carácter y recorre el resto del string.
+//Si encuentra un carácter diferente de n, no es un flag válido.
+//Todos los caracteres son n, se confirma como flag válido -n.
 
 int	ft_echo(char **args)
 {
@@ -43,6 +48,15 @@ int	ft_echo(char **args)
 	}
 	return (0);
 }
+//Función que simula el comando echo, con soporte para el flag -n.
+//is_flaged = Bandera para indicar si se detectó el flag -n.
+//Mientras haya flags -n, continúa avanzando.
+//Marca que se encontró al menos un flag -n.
+//Recorre los argumentos restantes.
+//Imprime el argumento actual a la salida estándar.
+//Si no es el último argumento, imprime un espacio.
+//Si no se usó el flag -n, añade un salto de línea.
+//Si no hay argumentos, no imprime nada y retorna 0.
 /*
 int	main(int argc, char **argv)
 {
