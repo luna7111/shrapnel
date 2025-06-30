d806441c357649debfa56fc570d7dc18983b43fd
luna <luna7111@tuta.io>
2025-06-30 21:38:27 +0200

    Command execution closes all fds opened for the next commands in command children

--- src/execution/execute.c
+++ src/execution/execute.c
@@ -6,12 +6,25 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/06/23 21:40:11 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/06/23 21:40:11 by ldel-val          ``                     */
+/*   Updated: 2025/06/30 21:33:32 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 #include "execute.h"
+
+void	clear_next_fds(t_redir *execlist)
+{
+	execlist++;	
+	while (execlist->flag != RE_END)
+	{
+		if (execlist->fd_in > 2)
+			close(execlist->fd_in);
+		if (execlist->fd_out > 2)
+			close(execlist->fd_out);
+		execlist++;
+	}
+}
 
 void	execute_command(t_data *data, t_redir *execlist)
 {
